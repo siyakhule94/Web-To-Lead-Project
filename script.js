6faf209a139cddcc0a6282a9706ed929d347f752
siyakhule94 <siyakhule94@gmail.com>
2025-12-05 15:06:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function beforesubmit()
+{
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate.value",inputdate.value);
+
+}
